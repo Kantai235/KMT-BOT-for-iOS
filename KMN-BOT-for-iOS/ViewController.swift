f1c6dcf2cde69b85c517590d64f7e58c0df52813
Huevo 蛋 <huevo235@gmail.com>
2016-09-19 16:57:09 +0800

    Initial Commit

---
+++ KMN-BOT-for-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KMN-BOT-for-iOS
+//
+//  Created by 熊谷乾太 on 2016/9/19.
+//  Copyright © 2016年 乾太. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
